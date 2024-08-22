a3d2c4c3b0a953269215937855b58a9d69e68c18
Tadlan, Jaspher Lloyd C <jasphertadlan@gmail.com>
2024-08-22 12:02:14 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+first_user_input = int(input("Enter first: "))
+
+second_user_input = int(input("Enter second number: "))
+
+
+if first_user_input < second_user_input:
+    print(f"{first_user_input} is less than {second_user_input}")
+
+elif first_user_input > second_user_input:
+    print(f"{first_user_input} is greater than {second_user_input}")
+
+elif first_user_input == second_user_input:
+    print(f"{first_user_input} is equal to {second_user_input}")
+
+
+# name1 = input("Enter classmate 1: ")
+# name2 = input("Enter classmate 2: ")
+# name3 = input("Enter classmate 3: ")
+#
+# print(f"Your classmate are: {name1}, {name2}, {name3}.")
